2e89cfcbdaa0b8446e959d167602adc55a379444
Emmatshi <etshi8712@gmail.com>
2023-04-28 18:44:20 -0500

    Adding dynamic pagination buttons

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
 	Product.find()
@@ -42,10 +42,10 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-	const page = req.query.page;
+	const page = +req.query.page || 1;
 	let totalItems;
 	Product.find()
-		.count()
+		.countDocuments()
 		.then((numProducts) => {
 			totalItems = numProducts;
 			return Product.find()
@@ -57,7 +57,7 @@ exports.getIndex = (req, res, next) => {
 				prods: products,
 				pageTitle: 'Shop',
 				path: '/',
-				totalProducts: totalItems,
+				currentPage: page,
 				hasNextPage: ITEMS_PER_PAGE * page < totalItems,
 				hasPreviousPage: page > 1,
 				nextPage: page + 1,
